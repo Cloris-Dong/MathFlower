53010b45665309f7503385131d4fbf7d3f44107a
JasmineGreenTea <91932332+Cloris-Dong@users.noreply.github.com>
2022-10-20 00:20:26 -0500

    initial documents

---
+++ js/Mathflower.js
@@ -0,0 +1,9 @@
+function setup() {
+  let cnv = createCanvas(innerWidth, innerHeight);
+}
+
+
+function windowResized() {
+   resizeCanvas(windowWidth, windowHeight);
+  }
+  //this is to readjust the window when refresh is not pressed;
